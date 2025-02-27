a0db778158dc1ec43878837949a26ab26cbc75f6
Sam Hollow <33451253+sambnh@users.noreply.github.com>
2025-02-27 20:20:55 +0000

    Draw hardcoded islands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::{HashMap, HashSet};
 
-use eframe::egui::{
-    self, text::LayoutJob, Align, Color32, FontFamily, FontId, Pos2, Rounding, TextFormat, Vec2,
-};
+use eframe::{egui::{
+    self, pos2, Color32, Label, Sense, Shape, Stroke, Vec2 
+}, epaint::TextShape};
 
 const ISLAND_SIZE: f32 = 50.0;
 
@@ -34,33 +34,39 @@ impl HashiBoard {
     }
 }
 
-impl eframe::App for HashiBoard {
+impl eframe::App for HashiBoard { 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.add(
-                egui::Button::new({
-                    let mut job = LayoutJob {
-                        halign: Align::Center,
-                        ..Default::default()
-                    };
-                    job.append(
-                        "1",
-                        0.0,
-                        TextFormat {
-                            font_id: FontId::new(14.0, FontFamily::Proportional),
-                            color: Color32::BLACK,
-                            ..Default::default()
-                        },
-                    );
-                    job
-                })
-                .rounding(Rounding::same(50.0))
-                .min_size(Vec2 {
-                    x: ISLAND_SIZE,
-                    y: ISLAND_SIZE,
-                })
-                .fill(Color32::WHITE),
-            );
+            let (response, painter) =
+            ui.allocate_painter(Vec2::new(ui.available_width(), 300.0), Sense::hover());
+
+            let mut control_points =  [
+                pos2(50.0, 50.0),
+                pos2(100.0, 100.0),
+                pos2(50.0, 100.0),
+            ];
+            let control_point_radius = 8.0;
+
+            let control_point_shapes: Vec<Shape> = 
+            control_points
+            .iter_mut()
+            .map(| point| {
+
+                let label = Label::new("1").sense(Sense::click());
+                let (_, galley, _) = label.layout_in_ui(ui);
+                let text_pos = *point - Vec2::new(galley.size().x / 2.0, galley.size().y / 2.0);
+
+                vec![
+                    Shape::circle_stroke(*point, control_point_radius, Stroke::new(1.0, Color32::BLACK)),
+                    Shape::Text(TextShape::new(text_pos, galley, Color32::BLACK))
+                ]
+            })
+            .flatten()
+            .collect();
+
+            painter.extend(control_point_shapes);
+
+            response
         });
     }
 }
